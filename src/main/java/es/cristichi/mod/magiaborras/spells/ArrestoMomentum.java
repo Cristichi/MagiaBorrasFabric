1a9c8cd269b6eec54b0934dfb44f4a13632b2d18
Cristichi <cristichi@hotmail.es>
2024-07-05 19:58:38 +0200

    QOL change. Instead of stopping gravity it grants levitation for a small time.

--- src/main/java/es/cristichi/mod/magiaborras/spells/ArrestoMomentum.java
+++ src/main/java/es/cristichi/mod/magiaborras/spells/ArrestoMomentum.java
@@ -5,6 +5,9 @@ import es.cristichi.mod.magiaborras.items.wand.prop.WandProperties;
 import es.cristichi.mod.magiaborras.spells.prop.SpellCastType;
 import es.cristichi.mod.magiaborras.util.EasyList;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -19,18 +22,18 @@ public class ArrestoMomentum extends Spell {
 
     public ArrestoMomentum() {
         super("arresto", Text.translatable("magiaborras.spell.arresto"), new EasyList<>(SpellCastType.USE),
-                Spell.NO_ENTITY, Spell.NO_BLOCK, null, 5);
+                Spell.NO_ENTITY, Spell.NO_BLOCK, null, 20);
     }
 
     @Override
     public Result use(ItemStack wand, WandProperties properties, PlayerEntity magicUser, World world, HitResult hit) {
         List<Entity> entities = world.getOtherEntities(null, magicUser.getBoundingBox().expand(area));
         for(Entity entity : entities){
-            boolean gravity = entity.hasNoGravity();
-            entity.setNoGravity(true);
             entity.setVelocity(0, 0, 0);
             entity.fallDistance = 0;
-            entity.setNoGravity(gravity);
+            if (entity instanceof LivingEntity livingEntity){
+                livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, baseCooldown/2));
+            }
         }
         return new Result(TypedActionResult.success(wand), baseCooldown, new EasyList<>(MagiaBorras.ARRESTO_CAST));
     }
