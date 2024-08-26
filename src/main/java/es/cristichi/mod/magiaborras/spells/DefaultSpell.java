2a8a6fa828bdc14270e850e94cda627d27ef0154
Cristichi <cristichi@hotmail.es>
2024-08-26 17:35:48 +0200

    Fixed and improved particles for DefaultSpell.java.
    Now it works, and also I remade the system so that it's now more consistent across particles.

--- src/main/java/es/cristichi/mod/magiaborras/spells/DefaultSpell.java
+++ src/main/java/es/cristichi/mod/magiaborras/spells/DefaultSpell.java
@@ -2,10 +2,14 @@ package es.cristichi.mod.magiaborras.spells;
 
 import es.cristichi.mod.magiaborras.items.wand.prop.WandProperties;
 import es.cristichi.mod.magiaborras.spells.prop.SpellCastType;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.particle.SimpleParticleType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
@@ -13,6 +17,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 
+import java.util.Collection;
 import java.util.List;
 
 public class DefaultSpell extends Spell {
@@ -24,55 +29,53 @@ public class DefaultSpell extends Spell {
 
     @Override
     public Result use(ItemStack wand, WandProperties properties, PlayerEntity magicUser, World world, HitResult hit) {
+        float power = properties.getPower(magicUser);
+        int count = 100 + (int) (power*200);
         SoundEvent sound = SoundEvents.ENTITY_ENDER_DRAGON_GROWL;
-        if (world.isClient()) {
-            float power = properties.getPower(magicUser);
-            double variation = 2 * power + 1;
-            int count = 100 + (int) (200 * power);
-            SimpleParticleType particle = ParticleTypes.DRAGON_BREATH;
-            if (power < 0.1) {
-                particle = ParticleTypes.ASH;
-                sound = SoundEvents.BLOCK_DISPENSER_FAIL;
-            } else if (power < 0.2) {
-                particle = ParticleTypes.FALLING_WATER;
-                sound = SoundEvents.BLOCK_DISPENSER_FAIL;
-            } else if (power < 0.3) {
-                particle = ParticleTypes.DRIPPING_WATER;
-                sound = SoundEvents.AMBIENT_UNDERWATER_ENTER;
-            } else if (power < 0.4) {
-                particle = ParticleTypes.UNDERWATER;
-                sound = SoundEvents.AMBIENT_UNDERWATER_ENTER;
-            } else if (power < 0.5) {
-                particle = ParticleTypes.FALLING_LAVA;
-                sound = SoundEvents.BLOCK_LAVA_POP;
-            } else if (power < 0.6) {
-                particle = ParticleTypes.DRIPPING_LAVA;
-                sound = SoundEvents.BLOCK_LAVA_POP;
-            } else if (power < 0.7) {
-                particle = ParticleTypes.FLAME;
-                sound = SoundEvents.ENTITY_PLAYER_HURT_ON_FIRE;
-            } else if (power < 0.8) {
-                particle = ParticleTypes.EGG_CRACK;
-                sound = SoundEvents.BLOCK_COMPOSTER_FILL_SUCCESS;
-            } else if (power < 0.9) {
-                particle = ParticleTypes.FIREWORK;
-                sound = SoundEvents.ENTITY_FIREWORK_ROCKET_BLAST;
-            } else if (power < 1) {
-                particle = ParticleTypes.CHERRY_LEAVES;
-                sound = SoundEvents.BLOCK_CHERRY_LEAVES_PLACE;
-                count = (int) (count * 1.5);
-            } else {
-                //particle = ParticleTypes.DRAGON_BREATH;
-                //sound = SoundEvents.ENTITY_ENDER_DRAGON_GROWL;
-                count = count * 2;
-            }
-            for (int i = 0; i < count; i++) {
-                double varX = Math.random() * variation * 2 - variation;
-                double varY = Math.random() * variation * 2 - variation;
-                double varZ = Math.random() * variation * 2 - variation;
-                world.addParticle(particle, magicUser.getX() + varX, magicUser.getY() +
-                        magicUser.getEyeHeight(magicUser.getPose()) + varY, magicUser.getZ() + varZ, 0, 0, 0);
-            }
+        SimpleParticleType particle = ParticleTypes.DRAGON_BREATH;
+        if (power < 0.1) {
+            particle = ParticleTypes.ASH;
+            sound = SoundEvents.BLOCK_DISPENSER_FAIL;
+        } else if (power < 0.2) {
+            particle = ParticleTypes.FALLING_WATER;
+            sound = SoundEvents.BLOCK_DISPENSER_FAIL;
+        } else if (power < 0.3) {
+            particle = ParticleTypes.DRIPPING_WATER;
+            sound = SoundEvents.AMBIENT_UNDERWATER_ENTER;
+        } else if (power < 0.4) {
+            particle = ParticleTypes.UNDERWATER;
+            sound = SoundEvents.AMBIENT_UNDERWATER_ENTER;
+        } else if (power < 0.5) {
+            particle = ParticleTypes.FALLING_LAVA;
+            sound = SoundEvents.BLOCK_LAVA_POP;
+        } else if (power < 0.6) {
+            particle = ParticleTypes.DRIPPING_LAVA;
+            sound = SoundEvents.BLOCK_LAVA_POP;
+        } else if (power < 0.7) {
+            particle = ParticleTypes.FLAME;
+            sound = SoundEvents.BLOCK_FIRE_AMBIENT;
+        } else if (power < 0.8) {
+            particle = ParticleTypes.EGG_CRACK;
+            sound = SoundEvents.BLOCK_AMETHYST_BLOCK_STEP;
+        } else if (power < 0.9) {
+            particle = ParticleTypes.FIREWORK;
+            sound = SoundEvents.ENTITY_FIREWORK_ROCKET_LARGE_BLAST;
+        } else if (power < 1) {
+            particle = ParticleTypes.CHERRY_LEAVES;
+            sound = SoundEvents.BLOCK_CHERRY_LEAVES_PLACE;
+        }
+
+        double maxVariation = 1 + power*4;
+        float var = (float) (Math.random() * maxVariation);
+        Collection<ServerPlayerEntity> players = PlayerLookup.tracking((ServerWorld) world, magicUser.getBlockPos());
+        ParticleS2CPacket packet = new ParticleS2CPacket(particle,
+                true, magicUser.getX(), magicUser.getY(), magicUser.getZ(),
+                var, var, var,
+                0, count);
+
+//        ((ServerPlayerEntity) magicUser).networkHandler.sendPacket(packet);
+        for (ServerPlayerEntity player : players){
+            player.networkHandler.sendPacket(packet);
         }
         return new Result(ActionResult.SUCCESS, baseCooldown, List.of(sound));
     }
